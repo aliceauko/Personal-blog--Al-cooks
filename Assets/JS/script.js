463b70da435a3db269f23443f5b5ec9227bdb399
aliceauko <alice.auko@student.moringaschool.com>
2022-06-04 00:40:30 +0300

    Change the display of the paragraphs to noe so as when you press the button they appear

---
+++ Assets/JS/script.js
@@ -0,0 +1,19 @@
+
+const hide = document.querySelector('.hide');
+const begin =document.querySelector('#mainButton');
+begin.addEventListener('click', (e) => {
+    e.preventDefault();
+    begin.style.display = 'block';
+    hide.style.display = 'block';
+  });
+
+
+const hid = document.querySelector('.hid');
+const beginer =document.querySelector('#mainButton');
+beginer.addEventListener('click', (e) => {
+    e.preventDefault();
+    beginer.style.display = 'block';
+    hid.style.display = 'block';
+  });
+
+
